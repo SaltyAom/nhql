5f817880c4aefbf7b280971d04e9abe1caa4f315
SaltyAom <aomkirby123@gmail.com>
2021-08-12 00:31:57 +0700

    :tada: feat: actix-cors

--- code/src/main.rs
+++ code/src/main.rs
@@ -4,13 +4,14 @@ mod modules;
 mod models;
 mod services;
 
-use actix_web::{ HttpServer, App, middleware::Compress, web::{ route, Data } };
+use actix_web::{ HttpServer, App, middleware::Compress, web::{ route, Data }, http };
 
 use modules::{
     status::controller::{ status, fallback },
     proxy::controller::proxy,
     graphql::controller::graphql
 };
+use actix_cors::Cors;
 
 use services::schema::service::create_schema;
 
@@ -21,7 +22,15 @@ async fn main() -> std::io::Result<()> {
     let schema = Arc::new(create_schema());
 
     HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin()
+            .send_wildcard()
+            .allowed_methods(vec!["GET", "POST"])
+            .allowed_headers(vec![http::header::CONTENT_TYPE, http::header::ACCEPT])
+            .max_age(86400);
+        
         App::new()
+        .wrap(cors)
             .wrap(Compress::default())
             .app_data(Data::new(schema.clone()))
             .configure(status)
